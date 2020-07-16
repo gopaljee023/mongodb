357d2abe74b27ff122b1d77bc0a09f935644cb57
gopal <gopaljee023@gmail.com>
2020-07-16 21:28:40 +0530

    connected mongoclient and printed admin related methods.

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-// const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
 const {MongoClient} = require('mongodb');
+const assert = require('assert');
 
-const url = 'mongodb://localhost:27017'
+const url = 'mongodb://localhost:27017';
 
 const dbName = 'circulation';// if doesn't persent, mongodb will create one for you
 
-(async function main(){
+(async function main() {
+ 
+  let client;
+  try {
+    console.log('Trying to connect:');
+    client =  await MongoClient.connect(url);
+    console.log('Connected correctly')
+    const admin = await client.db(dbName).admin();
+   
+    console.log(await admin.serverStatus());
+    console.log(await admin.listDatabases());
+  }
+  catch (err) {
+    console.log(err.stack);
+  }
+})();
 
-    const client = new MongoClient();
-    await client.connect();
-}());
 
-main();
