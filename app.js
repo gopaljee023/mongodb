34b62045628ebf403db94ad90e04aeb5d4611dc0
gopal <gopaljee023@gmail.com>
2020-07-20 15:56:40 +0530

    find(), insertOne,deleteOne,findOneAndReplace,insertMany use..

--- app.js
+++ app.js
@@ -1,52 +1,79 @@
-//const MongoClient = require('mongodb').MongoClient;
-const {MongoClient} = require('mongodb');
-const circulationRepo = require('./repos/circulationRepo');
-const data = require('./circulation.json');
+const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+const circulationRepo = require('./repos/circulationRepo');
+const data = require('./circulation.json')
+
 const url = 'mongodb://localhost:27017';
+const dbName = 'circulation';
 
-const dbName = 'circulation1';// if doesn't persent, mongodb will create one for you
+async function main() {
+  const client = new MongoClient(url);
+  await client.connect();
 
-(async function main() {
- 
-  let client;
   try {
-    console.log('Trying to connect:');
-    client =  await MongoClient.connect(url);
-    console.log('Connected correctly')
+    const results = await circulationRepo.loadData(data);
+    assert.equal(data.length, results.insertedCount);
 
-  
-    const result = await circulationRepo.loadData(data); 
-    assert.equal(data.length, result.insertedCount);  //checking inserted data equals data.length or not
+    const getData = await circulationRepo.get();
+    assert.equal(data.length, getData.length);
 
-    // I missed await here.. await is required since getData() is a promise
-    const newsPappers =  await circulationRepo.getData();
-    //console.log(getDatas);
-    assert.equal(data.length, newsPappers.length);
+    const filterData = await circulationRepo.get({ Newspaper: getData[4].Newspaper });
+    assert.deepEqual(filterData[0], getData[4]);
 
-    const filteredData =  await circulationRepo.getData({"Newspaper": "Wall Street Journal"});
-    console.log(filteredData);
-    assert.deepEqual(newsPappers[1], filteredData[0]);
+    const limitData = await circulationRepo.get({}, 3);
+    assert.equal(limitData.length, 3);
 
-    const filteredData1 =  await circulationRepo.getData({},3);
-    console.log(filteredData1);
-    assert.equal(filteredData1.length, 3);
+    const id = getData[4]._id.toString();
+    const byId = await circulationRepo.getById(id);
+    assert.deepEqual(byId, getData[4])
 
-  
+    const newItem = {
+      "Newspaper": "My paper",
+      "Daily Circulation, 2004": 1,
+      "Daily Circulation, 2013": 2,
+      "Change in Daily Circulation, 2004-2013": 100,
+      "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+      "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0
+    }
+    const addedItem = await circulationRepo.add(newItem);
+    assert(addedItem._id)
+    const addedItemQuery = await circulationRepo.getById(addedItem._id);
+    assert.deepEqual(addedItemQuery, newItem)
+
+    const updatedItem = await circulationRepo.update(addedItem._id, {
+      "Newspaper": "My new paper",
+      "Daily Circulation, 2004": 1,
+      "Daily Circulation, 2013": 2,
+      "Change in Daily Circulation, 2004-2013": 100,
+      "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+      "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0
+    });
+    assert.equal(updatedItem.Newspaper, "My new paper");
+
+
+    const newAddedItemQuery = await circulationRepo.getById(addedItem._id);
+    assert.equal(newAddedItemQuery.Newspaper, "My new paper");
+
+    const removed = await circulationRepo.remove(addedItem._id);
+    assert(removed);
+    const deletedItem = await circulationRepo.getById(addedItem._id);
+    assert.equal(deletedItem, null);
+  } catch (error) {
+    console.log(error)
+  } finally {
+    const admin = client.db(dbName).admin();
+
+    await client.db(dbName).dropDatabase();
+    console.log(await admin.listDatabases());
+
+    client.close();
   }
-  catch (err) {
-    console.log(err.stack);
-  }
-  finally{
-    const admin = await client.db(dbName).admin();
-    //  console.log(await admin.serverStatus());
-     console.log(await admin.listDatabases());
-     //here we finally dropping the db to see our test case.. not in production code
-     await client.db(dbName).dropDatabase();
-     //when client is not needed.. we are closing it.
-     client.close();
-  }
-})();
 
 
+
+}
+
+main();
